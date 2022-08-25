ded35dfe0b3fbe6c384d9adb62fa2f8e75d9b62e
LuigiDiS <luigi.distefano69@gmail.com>
2022-08-25 14:53:51 +0200

    Repo initialized

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const app = new Vue ({
+    el: '#root',
+    data: {
+        toDoList : toDoList,
+    },
+    methods: {
+        
+    },
+})
+
+toDoList = [
+    {
+        text: '',
+        done: '',
+    },
+    {
+        text: '',
+        done: '',
+    },
+    {
+        text: '',
+        done: '',
+    },
+    {
+        text: '',
+        done: '',
+    },
+    {
+        text: '',
+        done: '',
+    },
+]
