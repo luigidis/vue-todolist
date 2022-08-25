ea98610fb9491de639a7a540c59475abcbb12962
LuigiDiS <luigi.distefano69@gmail.com>
2022-08-25 15:27:07 +0200

    Milestone 1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
+toDoList = [
+    {
+        text: 'Lavare i Piatti',
+        done: true,
+    },
+    {
+        text: 'Fare la lavatrice',
+        done: false,
+    },
+    {
+        text: 'Fare la spesa',
+        done: false,
+    },
+    {
+        text: 'Andare a comprare le batterie',
+        done: false,
+    },
+    {
+        text: 'Prendere il pane',
+        done: false,
+    },
+]
+
 const app = new Vue ({
     el: '#root',
     data: {
@@ -8,25 +31,3 @@ const app = new Vue ({
     },
 })
 
-toDoList = [
-    {
-        text: '',
-        done: '',
-    },
-    {
-        text: '',
-        done: '',
-    },
-    {
-        text: '',
-        done: '',
-    },
-    {
-        text: '',
-        done: '',
-    },
-    {
-        text: '',
-        done: '',
-    },
-]
